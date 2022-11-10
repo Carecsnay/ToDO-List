15d17f1f178f5f0c096be5f13271c69e0f076b7d
carecsnay1@gmail.com <carecsnay1@gmail.com>
2022-11-10 15:18:37 -0300

    Function Create New Task added

--- script.js
+++ script.js
@@ -1,4 +1,112 @@
-// capturando os inputs e salvando em variáveis
+// Capturando os inputs e salvando em variáveis
 
-const newTask = document.querySelector('#task');
-const addNewTask = document.querySelector('.form input[type=submit]');
+const list = document.querySelector('ul');
+const fieldEnterNewTask = document.querySelector('#task');
+const buttonAddNewTask = document.querySelector('.form input[type=submit]');
+const buttonRemovesTask = document.querySelector('.remTask');
+const buttonFinishTask = document.querySelector('.finTask');
+const changeTheme = document.querySelector('.checkbox');
+
+// Testando os elementos selecionados
+
+// fieldEnterNewTask.addEventListener('click', teste);
+// buttonAddNewTask.addEventListener('click', teste);
+// buttonRemovesTask.addEventListener('click', teste);
+// buttonFinishTask.addEventListener('click', teste);
+// changeTheme.addEventListener('click', teste);
+
+// function teste(){
+//     alert('clicou');
+// }
+
+// Funcionalidades da aplicação
+
+buttonAddNewTask.addEventListener('click', addNewTask);
+
+function addNewTask() {
+    const typedContent = fieldEnterNewTask.value;
+
+    // Criando os elementos
+
+    // Replicando essa estrutura aqui
+    // <li>
+    //     <h2>Tarefa 02</h2>
+    //     <div>
+    //         <button>
+    //             <img class="finTask" src="./assets/square.svg" alt="Icone de concluir Tarefa">
+    //         </button>
+    //         <button>
+    //             <img class="remTask" src="./assets/trash.svg" alt="Icone de remoção de Tarefa">
+    //         </button>
+    //     </div>                              
+    // </li>
+
+
+    // 1º método para replicar a estrutura linha 45 até 75
+    // const li = document.createElement('li');
+    // const h2 = document.createElement('h2');
+    // const div = document.createElement('div');
+    // const finTask = document.createElement('button');
+    // const removeTask = document.createElement('button');
+    // const img = document.createElement('img');
+    // const img2 = document.createElement('img');
+
+    // // Montando os elementos na página
+
+    // // Inserindo valor dentro da tag
+    // h2.innerText = typedContent;
+
+    // // Adicionando imagem e classe de concluir tarefa
+    // img.src = './assets/square.svg';
+    // img.alt = 'Icone de concluir Tarefa';
+    // img.classList.add('finTask');
+
+    // // Adicionando imagem e classe de excluir tarefa
+    // img2.src = './assets/trash.svg';
+    // img2.alt = 'Icone de remoção de Tarefa';
+    // img2.classList.add('remTask');
+
+    // //Inserindo elementos aninhados
+
+    // li.appendChild(h2); //h2 filho do li
+    // li.appendChild(div); //div filha do li
+    // div.appendChild(finTask); //botão concluir tarefa
+    // div.appendChild(removeTask); //botão remover tarefa
+    // finTask.appendChild(img); //img filha do botão finTask
+    // removeTask.appendChild(img2); //img filha do botão remTask
+
+
+    // 2º método para replicar a estrutura linha 79 até 
+    const newTaskStructure = `
+        <h2>${typedContent}</h2>
+        <div>
+            <button>
+                <img class="finTask" src="./assets/square.svg" alt="Icone de concluir Tarefa">
+            </button>
+            <button>
+                <img class="remTask" src="./assets/trash.svg" alt="Icone de remoção de Tarefa">
+            </button>
+        </div>                              
+    `;
+
+    // Cria o elemento LI
+    const taskToBeCreated = document.createElement('li');
+    
+    // Interpreta a constante "newTaskStructure" como HTML
+    taskToBeCreated.innerHTML = newTaskStructure; 
+
+    // Joga a estrutura interpretada dentro do LI
+    list.appendChild(taskToBeCreated);
+}
+
+// function finishTask() {
+
+// }
+
+// function removeTask() {
+
+// }
+
+// function changeTheme() {
+
+// }
